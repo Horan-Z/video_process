600bead554bda0084512a8714cfc82e03990e524
Horan-Z <i17611745217@gmail.com>
2025-05-26 12:50:50 +0800

    initial commit

---
+++ vp-backend/src/main/java/com/vp/vpbackend/VpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vp.vpbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VpBackendApplication.class, args);
+    }
+
+}
---
+++ vp-backend/src/test/java/com/vp/vpbackend/VpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vp.vpbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
