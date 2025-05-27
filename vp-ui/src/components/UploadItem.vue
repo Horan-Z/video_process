453486e8d4280b2364a4eded5b2be1ed12749879
Horan-Z <i17611745217@gmail.com>
2025-05-27 14:20:26 +0800

    修改请求头

--- vp-ui/src/components/UploadItem.vue
+++ vp-ui/src/components/UploadItem.vue
@@ -77,9 +77,9 @@ const headers = {
   // 指定Object的访问权限。
   'x-oss-object-acl': 'private',
   // 通过文件URL访问文件时，指定以附件形式下载文件，下载后的文件名称定义为example.txt。
-  'Content-Disposition': 'attachment; filename="example.txt"',
+  // 'Content-Disposition': 'attachment; filename="example.txt"',
   // 设置Object的标签，可同时设置多个标签。
-  'x-oss-tagging': 'Tag1=1&Tag2=2',
+  // 'x-oss-tagging': 'Tag1=1&Tag2=2',
   // 指定PutObject操作时是否覆盖同名目标Object。此处设置为true，表示禁止覆盖同名Object。
   'x-oss-forbid-overwrite': 'true',
 }
