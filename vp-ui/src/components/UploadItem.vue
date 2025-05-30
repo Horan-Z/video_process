af883369e5434441a708b9b814a14522f102ff87
Horan-Z <i17611745217@gmail.com>
2025-05-30 11:37:27 +0800

    多文件上传

--- vp-ui/src/components/UploadItem.vue
+++ vp-ui/src/components/UploadItem.vue
@@ -2,94 +2,23 @@
 import { UploadFilled, Delete } from '@element-plus/icons-vue'
 import OSS from 'ali-oss'
 import axios from 'axios'
-import { ref } from 'vue'
+import { reactive, ref } from 'vue'
+import { v4 as uuidv4 } from 'uuid';
 
-const uploadPercentage = ref(0)
-const inProgress = ref(false)
-const uploadFinish = ref(false)
-const uploadRef = ref(null)
-const fileName = ref('null')
-
-class OSSClientManager {
-  private static instance: OSSClientManager
-  private client: OSS | null = null
-  private region = 'oss-cn-beijing'
-  private bucket = 'xiaoming10086'
-  private stsEndpoint = 'http://localhost:8080/api/sts'
-
-  // 单例模式，确保全局只有一个client实例
-  public static getInstance(): OSSClientManager {
-    if (!OSSClientManager.instance) {
-      OSSClientManager.instance = new OSSClientManager()
-    }
-    return OSSClientManager.instance
-  }
-
-  // 初始化或获取已有的client
-  public async getClient(): Promise<OSS> {
-    if (this.client) {
-      return this.client
-    }
-
-    const token = await this.fetchSTSToken()
-    this.client = new OSS({
-      region: this.region,
-      accessKeyId: token.credentials.accessKeyId,
-      accessKeySecret: token.credentials.accessKeySecret,
-      stsToken: token.credentials.securityToken,
-      // @ts-expect-error idk
-      authorizationV4: true,
-      bucket: this.bucket,
-      refreshSTSToken: async () => {
-        const refreshedToken = await this.fetchSTSToken()
-        return {
-          accessKeyId: refreshedToken.credentials.accessKeyId,
-          accessKeySecret: refreshedToken.credentials.accessKeySecret,
-          stsToken: refreshedToken.credentials.securityToken,
-        }
-      },
-    })
-    return this.client
-  }
-
-  // 上传文件方法
-  public async uploadFile(fileName: string, file: File) {
-    try {
-      const client = await this.getClient()
-      const result = await client.multipartUpload(fileName, file, {
-        parallel: 4,
-        partSize: 5 * 1024 * 1024, // 5MByte
-        progress,
-        headers,
-      })
-      console.log('上传成功:', result)
-    } catch (error) {
-      console.error('上传失败:', error)
-      throw error
-    }
-  }
-
-  public async abortUpload() {
-    try {
-      const client = await this.getClient()
-      // @ts-expect-error it does exist
-      await client.cancel()
-    } catch (error) {
-      throw error
-    }
-  }
-
-  // 获取STS令牌
-  private async fetchSTSToken() {
-    const response = await axios.get(this.stsEndpoint)
-    return response.data.data
-  }
+interface UploadFile {
+  fileUuid: string;     // 唯一标识
+  fileName: string;     // 文件名
+  percentage: number;   // 上传进度
+  checkpoint: object | null;   // 分片上传checkpoint
+  inProgress: boolean;  // 是否上传中
+  uploadFinish: boolean;// 是否上传完成
+  file: File;           // 文件对象
+  ossObject: string;
+  canceled: boolean;
+  client: OSS;
 }
 
-const progress = (p: number) => {
-  console.log(p)
-  uploadPercentage.value = parseFloat((p * 100).toFixed(2))
-}
+const uploadFiles = ref<UploadFile[]>([]);
 
 // 自定义请求头
 const headers = {
@@ -102,53 +31,101 @@ const headers = {
   // 设置Object的标签，可同时设置多个标签。
   // 'x-oss-tagging': 'Tag1=1&Tag2=2',
   // 指定PutObject操作时是否覆盖同名目标Object。此处设置为true，表示禁止覆盖同名Object。
-  'x-oss-forbid-overwrite': 'false',
+  'x-oss-forbid-overwrite': 'true',
 }
 
-async function upload(options: JSON) {
+async function createClient() {
+  const token = await fetchSTSToken()
+  return new OSS({
+    region: 'oss-cn-beijing',
+    accessKeyId: token.credentials.accessKeyId,
+    accessKeySecret: token.credentials.accessKeySecret,
+    stsToken: token.credentials.securityToken,
+    // @ts-expect-error idk
+    authorizationV4: true,
+    bucket: 'xiaoming10086',
+    refreshSTSToken: async () => {
+      const token = await fetchSTSToken()
+      return {
+        accessKeyId: token.credentials.accessKeyId,
+        accessKeySecret: token.credentials.accessKeySecret,
+        stsToken: token.credentials.securityToken,
+      }
+    },
+  })
+}
+
+// 获取STS令牌
+async function fetchSTSToken() {
+  const response = await axios.get('http://localhost:8080/api/sts')
+  return response.data.data
+}
+
+function getFileExtension(filename: string) {
+  const parts = filename.split('.');
+  return parts.length > 1 ? parts.pop() : '';
+}
+
+async function upload(options: object) {
   try {
-    // @ts-expect-error it does exist
-    const file = options.file
-    fileName.value = file.name
-    await OSSClientManager.getInstance().uploadFile('/vp/source/' + fileName.value, file)
-    console.log('上传完毕')
-    uploadSuccess()
+    const newFile = reactive<UploadFile>({
+      fileUuid: uuidv4(),
+      // @ts-expect-error it does exist
+      fileName: options.file.name,
+      percentage: 0,
+      checkpoint: null,
+      inProgress: true,
+      uploadFinish: false,
+      // @ts-expect-error it does exist
+      file: options.file,
+      ossObject: '',
+      canceled: false,
+      client: await createClient()
+    })
+    uploadFiles.value.push(newFile)
+    newFile.ossObject = `/vp/source/${newFile.fileUuid}.${getFileExtension(newFile.fileName)}`
+    const result = await newFile.client.multipartUpload(newFile.ossObject, newFile.file, {
+      // parallel: 1,
+      partSize: 5 * 1024 * 1024, // 5MByte
+      progress: (p: number, checkpoint: object) => {
+        newFile.percentage = parseFloat((p * 100).toFixed(2))
+        newFile.checkpoint = checkpoint
+      },
+      headers,
+    })
+    console.log('上传成功:', result)
+    uploadSuccess(newFile)
   } catch (e) {
     console.log(e)
   }
 }
 
-function uploadSuccess() {
-  inProgress.value = false
-  uploadFinish.value = true
+function uploadSuccess(newFile: UploadFile) {
+  newFile.inProgress = false
+  newFile.uploadFinish = true
+  newFile.percentage = 100.00
 }
 
-async function abortOrRemove() {
+async function abortOrRemove(file: UploadFile) {
   try{
-    if(inProgress.value) {
-      await OSSClientManager.getInstance().abortUpload()
-    } else if(uploadFinish.value) {
+    file.canceled = true
+    if(file.inProgress) {
+      // // @ts-expect-error it does exist
+      // await file.client.abortMultipartUpload(file.checkpoint.name, file.checkpoint.uploadId)
+      // @ts-expect-error it does exist
+      await file.client.cancel()
+    } else if(file.uploadFinish) {
       console.log('remove')
       // TODO remove logic
     }
-    // @ts-expect-error it does exist
-    uploadRef.value!.clearFiles()
-    uploadPercentage.value = 0.0
-    inProgress.value = false
-    uploadFinish.value = false
+    // 从列表中移除文件
+    const index = uploadFiles.value.findIndex(f => f.fileUuid === file.fileUuid);
+    if (index !== -1) {
+      uploadFiles.value.splice(index, 1);
+    }
   } catch(e) {
     console.log(e)
   }
-
-}
-
-function beforeUpload() {
-  uploadPercentage.value = 0.0
-  inProgress.value = true
-}
-
-function exceed() {
-  console.log('exceed')
 }
 </script>
 
@@ -157,31 +134,26 @@ function exceed() {
     class="upload"
     ref="uploadRef"
     drag
+    multiple
     action="#"
     :http-request="upload"
-    :before-upload="beforeUpload"
-    :on-remove="abortOrRemove"
-    :limit="1"
-    :on-exceed="exceed"
     :show-file-list="false"
     auto-upload
-    v-show="!(inProgress || uploadFinish)"
+    v-show="true"
   >
     <el-icon class="el-icon--upload"><upload-filled /></el-icon>
     <div class="el-upload__text">Drop file here or <em>click to upload</em></div>
     <div class="el-upload__text">最大文件大小2GB</div>
   </el-upload>
 
-  <div class="progress" v-show="inProgress || uploadFinish">
-    <div class="name">
-      <span style="font-size: 1.8em;">{{ fileName }}</span>
-      <el-button type="danger" :icon="Delete" circle @click="abortOrRemove" />
+  <div class="progress" v-for="file in uploadFiles" :key="file.fileUuid">
+    <div  v-if="!file.canceled">
+      <div class="name">
+        <span style="font-size: 1.8em;">{{ file.fileName }}</span>
+        <el-button type="danger" :icon="Delete" circle @click="abortOrRemove(file)" />
+      </div>
+      <el-progress :text-inside="true" :stroke-width="26" :percentage="file.percentage" :status="file.percentage == 100 ? 'success' : ''" />
     </div>
-    <el-progress :text-inside="true" :stroke-width="26" :percentage="uploadPercentage" />
-  </div>
-
-  <div class="settings" v-show="inProgress || uploadFinish">
-
   </div>
 </template>
 
